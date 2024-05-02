c27c7a261c65dd1c27060d2377f643e1fbf1ed4b
unknown <jgonzalo.hernandezy@gmail.com>
2024-05-01 18:46:35 -0600

    resta de 2 numeros

--- MainResta.java
+++ MainResta.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class MainResta {
     public static void main(String[] args) {
-        Scanner n1 = new Scanner(System.in);
+        Scanner num = new Scanner(System.in);
+        Resta n1 = new Resta();
         System.out.println("Ingresa un numero");
-        int a = n1.nextInt();
+        n1.setA(num.nextInt());
         System.out.println("Ingrese el segundo numero");
-        int b = n1.nextInt();
-        int r = a - b;
-        System.out.println("El resultado es:" + r);
+        n1.setB(num.nextInt());
+        System.out.println("El resultado es:");
+        System.out.println(n1.restar());
+        System.out.println(n1);
+        
+        n1.getA();
+        n1.getB();
 
     }
 }
--- Resta.java
+++ Resta.java
@@ -1,8 +1,36 @@
 public class Resta {
-    int a, b, r;
-
-    public int restar(int a, int b) {
+    private int a, b, r;
+    public int restar() {
         r = this.a - this.b;
         return r;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+    
+    @Override
+    public String toString() {
+        return "Resta [a=" + a + ", b=" + b + ", r=" + r + "]";
     }
 }
